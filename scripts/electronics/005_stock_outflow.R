b2ef4ae38a4078bf23d0bc64a8d44baac5b8ba3e
OliverLysa <oliverlysaght@icloud.com>
2023-06-14 22:07:29 -0600

    stock outflow and functions

--- scripts/electronics/005_stock_outflow.R
+++ scripts/electronics/005_stock_outflow.R
@@ -54,14 +54,42 @@ lifespan_data_filtered <- lifespan_data[c(1:54), c(1,7,8)] %>%
          scale = 3) %>%
   na.omit() 
 
-for (i in seq_along(unu_keys)) {
-  weibullparinv(lifespan_data_filtered$shape, lifespan_data_filtered$scale, loc = 0)
-}
-
 # Calculate mean and median from Weibull parameters
-# weibullparinv(1.6, 8.1599999951404, loc = 0)
+weibullparinv(1.6, 8.1599999951404, loc = 0)
+
+# Import inflow data
+inflow_unu_mass_units <- 
+  read_xlsx("./cleaned_data/inflow_unu_mass_units.xlsx")
+
+# Merge inflow and lifespan data by unu_key
+inflow_weibull <- merge(inflow_unu_mass_units, lifespan_data_filtered,  by=c("unu_key"),  
+                        all.x = TRUE)
+
+# Create empty columns for all years in range of interest
+year_first <- min(as.integer(inflow_weibull$year))
+year_last <- max(as.integer(inflow_weibull$year)) + 30
+
+years <- c(year_first:year_last)
+empty <- as.data.frame(matrix(NA, ncol = length(years), nrow = nrow(inflow_weibull)))
+colnames(empty) <- years
+
+# Add them to inflow weibull dataframe
+inflow_weibull <- cbind(inflow_weibull, empty)
+rm(empty)
+
+# Calculate WEEE from inflow year 
+for (i in year_first:year_last){
+  inflow_weibull$WEEE_POM_dif <- i - ( as.integer(inflow_weibull[, "year"]) )
+  wb <- dweibull(inflow_weibull[(inflow_weibull$WEEE_POM_dif >= 0),"WEEE_POM_dif"] + 0.5,
+                 shape = inflow_weibull[(inflow_weibull$WEEE_POM_dif >= 0), "shape"],
+                 scale = inflow_weibull[(inflow_weibull$WEEE_POM_dif >= 0), "scale"],
+                 log = FALSE)
+  weee <-  wb * inflow_weibull[(inflow_weibull$WEEE_POM_dif >= 0), "value"]
+  inflow_weibull[(inflow_weibull$WEEE_POM_dif >= 0), as.character(i)] <- weee
+}  
 
 ### Stock data: Electrical products data tables (represent an underestimate)
 
 # https://onlinelibrary.wiley.com/doi/abs/10.1111/jiec.12551
 # https://www.sciencedirect.com/science/article/abs/pii/S0959652618339660
+
--- scripts/functions.R
+++ scripts/functions.R
@@ -63,4 +63,19 @@ cdweibull <- function(x, shape, scale, log = FALSE){
   return(dd)
 }
 
-
+# From Weibull par inverse mixdist
+weibullparinv <- function(shape, scale, loc = 0) 
+{
+  nu <- 1/shape
+  if (nu < 1e-6) {
+    mu <- scale * (1 + nu * digamma(1) + nu^2 * (digamma(1)^2 + 
+                                                   trigamma(1))/2)
+    sigma <- scale^2 * nu^2 * trigamma(1)
+  }
+  else {
+    mu <- loc + gamma(1 + (nu)) * scale
+    sigma <- sqrt(gamma(1 + 2 * nu) - (gamma(1 + nu))^2) * 
+      scale
+  }
+  data.frame(mu, sigma, loc)
+}
