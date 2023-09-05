fe189d4e53c9be116e3a685ca33353911bd22a7b
OliverLysa <oliverlysaght@icloud.com>
2023-09-04 18:39:39 -0600

    textiles

--- scripts/textiles/sankey_chart.R
+++ scripts/textiles/sankey_chart.R
@@ -35,10 +35,62 @@ source("./scripts/functions.R",
 # Stop scientific notation of numeric values
 options(scipen = 999)
 
-# Import scenario sankey data 
+# *******************************************************************************
+# Import baseline sankey data 
+# *******************************************************************************
 
-# Write file 
-write_csv(sankey_all, 
-          "./cleaned_data/electronics_chart_sankey.csv")
+# *******************************************************************************
+# Import 2018 data and calculate variables 
 
+# Definitions: 
+# Inflow: filter target column to consumption
+# Outflow: filter target column to non-UK disposals and residual waste and sum
+
+# Manual extraction
+textiles_sankey_links_2018 <- read_csv(
+  "./raw_data/textiles/textiles_sankey_links.csv") %>%
+  filter() %>%
+  mutate(scenario = "BAU_v6")
+
+# Read inflow, stock and outflow data for 2018
+# Source: https://eprints.whiterose.ac.uk/198586/1/1-s2.0-S0959652623013161-main.pdf
+
+# Specify the file from which to extract the table
+pdf_file <- './raw_data/textiles/1-s2.0-S0959652623013161-main.pdf'
+
+# Extract data frame from pdf using shiny/miniUI (brings up interactive window to drag to table of choice)
+textiles_raw_2018 <- extract_areas(pdf_file, pages = 3) %>%
+  as.data.frame() %>%
+  select(-c(X4)) %>%
+  row_to_names(1)
+
+# If any cell in row blank, combine contents of non-empty cells with those above as a suffix
+
+# *******************************************************************************
+# Import scenario data
+
+# Import projected data, filter to destination in filter list above and
+textiles_sankey_projected <- read_excel(
+  "./raw_data/textiles/Outputs_ForDistribution.xlsx") %>%
+  rename(source = Origin,
+         target = Destination,
+         value = Value_kt) %>%
+  clean_names() %>%
+  mutate(product = "Textiles",
+         material = "Textiles")
+
+# Bind 2018 data and scenario data
+textiles_sankey <- rbindlist(
+  list(
+    textiles_sankey_links_2018,
+    textiles_sankey_projected),
+  use.names = TRUE) %>%
+  mutate(value = value*1000) %>%
+  mutate(across(c('value'), round, 1)) %>%
+  mutate(across(everything(), ~ replace(., . == "Non-UK reuse", "Reused non-UK"))) %>%
+  mutate(across(everything(), ~ replace(., . == "Non-UK disposals", "Disposed non-UK")))
+
+# Write csv
+write_csv(textiles_sankey,
+  "./cleaned_data/textiles_sankey_links.csv")
 
--- scripts/textiles/stacked_area_chart.R
+++ scripts/textiles/stacked_area_chart.R
@@ -20,7 +20,8 @@ packages <- c("magrittr",
               "data.table",
               "shiny",
               "miniUI",
-              "rvest")
+              "rvest",
+              "DataEditR")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -45,47 +46,32 @@ options(scipen = 999)
 # *******************************************************************************
 # Data extraction
 # *******************************************************************************
-
-# Definitions: 
-# Inflow: filter target column to consumption
-# Outflow: filter target column to non-UK disposals and residual waste and sum
-
-# To remove (manual extraction)
-textiles_sankey_links_2018 <- read_csv(
-  "./cleaned_data/textiles_sankey_links.csv") %>%
-  filter()
-
-# Read inflow, stock and outflow data for 2018
-# Source: https://eprints.whiterose.ac.uk/198586/1/1-s2.0-S0959652623013161-main.pdf
-
-# Specify the file from which to extract the table
-pdf_file <- './raw_data/textiles/1-s2.0-S0959652623013161-main.pdf'
-
-# Extract data frame from pdf using shiny/miniUI (brings up interactive window to drag to table of choice)
-textiles_raw_2018 <- extract_areas(pdf_file, pages = 3) %>%
-  as.data.frame() %>%
-  select(-c(X4)) %>%
-  row_to_names(1)
-
+  
 # *******************************************************************************
-# Import scenario data and extract variables
+# Extract variables
 
-# Variable filter list
+# Non-UK disposals vs. UK disposals
+# Recycled, UK reuse and non-UK reuse (aggregate)
+
+# Create filter list for variables
 filter_list <- c("Consumption",
                  "Non-UK disposals",
                  "Residual waste")
 
-# Import projected data, filter to destination in filter list above and
-textiles_sankey_projected <- read_excel(
-  "./raw_data/textiles/Outputs_ForDistribution.xlsx") %>%
-  filter(Destination %in% filter_list) %>%
+# Import sankey data 
+textiles_area <- read_csv(
+          "./cleaned_data/textiles_sankey_links.csv") %>%
+  filter(target %in% filter_list) %>%
   mutate(
-    Destination = gsub("Non-UK disposals", 'outflow', Destination),
-    Destination = gsub("Residual waste", 'outflow', Destination),
-    Destination = gsub("Consumption", 'inflow', Destination)) %>%
-  group_by(Destination, Year, Scenario) %>%
-  summarise(value = sum(Value_kt)) %>%
+    target = gsub("Non-UK disposals", 'outflow', target),
+    target = gsub("Residual waste", 'outflow', target),
+    target = gsub("Consumption", 'inflow',target)) %>%
+  group_by(target, year, scenario) %>%
+  summarise(value = sum(value)) %>%
   clean_names() %>%
   mutate(unit = "mass",
          product = "Textiles") %>%
-  rename(variable = destination)
+  rename(variable = target) %>%
+  write_csv("./cleaned_data/textiles_chart_area.csv")
+
+# Separate out outflows
